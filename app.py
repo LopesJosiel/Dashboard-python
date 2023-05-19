b286b681f1ff5001e020c47c6d261b854a178265
LopesJosiel <josielslopes@gmail.com>
2023-05-18 23:04:41 -0300

    Parte inicial do Dashboard gerada com sucesso

--- app.py
+++ app.py
@@ -1,43 +1,31 @@
 import pandas as pd
 import numpy as np
 from dash import Dash, dcc, html
+import plotly.express as px 
+app = Dash(__name__)
 #correção de erros até conseguir utilizar o ambiente virtual
 #agora definir a forma correta de acesso aos dados
-df1 = pd.read_excel('c:/Users/UNB/Anexos_Balanço_TD - Março 2023.xlsx').query("type == 'conventional' and region == 'Albany'")
-app = Dash(__name__)
-print (df1)
-app.layout = html.Div(
-    children=[
-        html.H1(children="Análise do Tesouro Direto"),
-        html.P(
-            children=(
-                "Venda por prazo"
-                "número de operações de venda por faixa de aplicação"
-            ),
-        ),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": data["Total"],
-                        "y": data["Porcentagem"],
-                       
-                    },
-                ],
-                "layout": {"title": "Average Price of Avocados"},
-            },
-        ),
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": data["Date"],
-                        "y": data["Total Volume"],
-                
-                    },
-                ],
-                "layout": {"title": "Avocados Sold"},
-            },
-        ),
-    ]
-)
+#procurando configuração adequada para que o dash gere o gráfico
+
+df = pd.DataFrame({
+    "meses": ['Mar/22','Abr/22','Mai/22','Jun/22','Jul/22','Ago/22','Set/22','Out/22','Nov/22','Dez/22','Jan/23', 'Fev/23', 'Mar/23'],
+    "Total": [ 17891025, 18392003, 18953067, 19492362, 20028345, 20665899, 21161249, 21600786, 22048922, 22483236, 23023837, 23355547, 23724147]
+})
+
+fig = px.bar(df, x="meses", y="Total")
+
+app.layout = html.Div(children=[
+    html.H1(children='Análise Tesouro Direto'),
+
+    html.Div(children='''
+        Tesouro: Total de investidores de março/2022 a março/2023.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
